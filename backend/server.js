bc37b903063f9a057524ef8663c11f9bd375bf14
senyoudev <younesmeskafe2018@gmail.com>
2023-01-11 18:13:19 +0100

    initial backend

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/connectDb.js';
+import cors from 'cors'
+import bodyParser from 'body-parser';
+
+
+const app = express();
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL || '*',
+		optionsSuccessStatus: 200,
+	}),
+);
+app.use(express.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	}),
+);
+app.use(
+	bodyParser.json({
+		limit: '4mb',
+	}),
+);
+dotenv.config();
+connectDb()
+
+// Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
