1eed78fd62b2d7172c718288b23772a1fb7fb31f
senyoudev <younesmeskafe2018@gmail.com>
2023-01-28 23:07:31 +0100

    setup the colors ..

---
+++ components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="fixed top-0 z-50 w-full py-4">
+        <nav className="container relative mx-auto">
+
+        </nav>
+    </header>
+  )
+}
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "primary-beige":"#F0F0FB",
+            "primary-purple":"#B4BAF9",
+            "light-purple":"#CDD5FB",
+            "primary-pink":"#E1D1F4",
+            "pinky-purple":"#C8B9F8",
+            "dark-purple":"#9400FF",
+            "black-purple":"#340072",
+            "primary-black":"#1A0532",
+            
+      },
+    },
   },
   plugins: [],
 }
