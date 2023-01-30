8aefaa93cf6c7bf8953c356b31c6da4be1902e04
senyoudev <younesmeskafe2018@gmail.com>
2023-01-30 01:03:30 +0100

    remove comments

--- components/cards/TrippleCard.tsx
+++ components/cards/TrippleCard.tsx
@@ -21,25 +21,7 @@ function TrippleCard({ elements }: any) {
           )}
         </>
       ))}
-      {/* 
-      <div className="md:w-1/3 flex flex-col items-center justify-around">
-        <h3 className="font-sahitya text-blackPurple text-xl font-bold text-center capitalize">
-          Purpose
-        </h3>
-        <p className="font-maven-pro text-primaryBlack w-3/4  leading-8 text-center capitalize">
-          CodeMavricks Dao aims to train 1m web3 Developper.
-        </p>
-      </div>
-      <div className="h-3/4 w-1 border-l border-darkPurple my-auto hidden md:block"></div>
-      <hr className="w-3/4 border-t border-solid border-darkPurple block md:hidden" />
-      <div className="md:w-1/3 flex flex-col items-center justify-around">
-        <h3 className="font-sahitya text-blackPurple text-xl font-bold text-center capitalize">
-          Purpose
-        </h3>
-        <p className="font-maven-pro text-primaryBlack w-3/4  leading-8 text-center capitalize">
-          CodeMavricks Dao aims to train 1m web3 Developper.
-        </p>
-      </div> */}
+
     </div>
   );
 }
