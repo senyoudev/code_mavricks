3e53df255806b71b75dea3607102d00b0da1aa20
NohailaElmasaoul <elmasaoulnohaila@gmail.com>
2023-01-31 20:40:28 +0000

    Community

---
+++ data/Community.ts
@@ -0,0 +1,15 @@
+export const Community = [
+    {
+      title: "Discord",
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      title: "Slack",
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+    },
+    {
+      title: "Github", 
+      desc: "Lorem ipsum dolor sit amet consectetur.",
+    },
+  ];
+  
