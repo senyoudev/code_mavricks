6aad272edd3da6893edf076b3b0b26ad4d5204e6
NohailaElmasaoul <elmasaoulnohaila@gmail.com>
2023-01-31 20:39:50 +0000

    Reasons

---
+++ data/Reasons.ts
@@ -0,0 +1,9 @@
+export const Reasons = [
+    {
+      title: "Code Mavericks",
+      desc: "CodeMavricks Dao aims to train 1m web3 Developer. \n The DAO could be structured in a way that allows CodeMavricks Dao aims to train 1m web3 Developer. \n The DAO could be structured in a way that allows community members to contribute resources, expertise, and ideas to the training program CodeMavricks Dao aims to train 1m web3 Developer. \n The DAO could be structured in a way that allows community members to contribute resources, expertise, and ideas to the training program CodeMavricks Dao aims to train 1m web3 Developer. \n The DAO could be structured in a way that allows community members to contribute resources, expertise, and ideas to the training program",
+    },
+    
+    
+  ];
+  
