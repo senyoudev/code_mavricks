f3599bf3039786a8b0f9e4f3c0f16eb9e46e0de8
senyoudev <younesmeskafe2018@gmail.com>
2023-02-02 23:41:56 +0100

    modal size

--- components/Modals/ConnectModal.jsx
+++ components/Modals/ConnectModal.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-
-const ConnectModal = ({ showModal, setShowModal,connectCoinbase,connectMetamask,connectWalletConnect }) => {
-  
-
+const ConnectModal = ({
+  showModal,
+  setShowModal,
+  connectCoinbase,
+  connectMetamask,
+  connectWalletConnect,
+}) => {
   return (
-    <div className="relative ">
+    <div className="relative">
       {showModal && (
-        <div className="fixed inset-0 overflow-hidden text-darkPurple font-maven-pro">
+        <div className="fixed inset-0 overflow-hidden text-darkPurple font-maven-pro ">
           <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
-          <div className="relative w-64 mx-auto my-10 bg-linearPurple rounded-lg p-6">
+          <div className="relative w-1/3 h-72 mx-auto my-10 bg-linearPurple rounded-lg p-6">
             <div className="flex items-center  justify-between border-b border-secondaryPurple pb-2">
               <h3 className="text-lg font-medium">Choose a Wallet</h3>
               <button
@@ -19,7 +22,7 @@ const ConnectModal = ({ showModal, setShowModal,connectCoinbase,connectMetamask,
                 Close
               </button>
             </div>
-            <div className="mt-4">
+            <div className="mt-4 flex flex-col items-center justify-between h-3/4">
               <button
                 className="w-full flex items-center justify-center p-2 border-2 border-secondaryPurple rounded hover:bg-white mt-2"
                 onClick={() => connectMetamask()}
