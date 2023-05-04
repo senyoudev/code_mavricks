bac8cb12a2b8e021a6423e3e62eefa6b0c0ccd9c
NohailaElmasaoul <elmasaoulnohaila@gmail.com>
2023-05-04 00:42:13 +0000

    nft

---
+++ data/nfts.ts
@@ -0,0 +1,19 @@
+export const nftsData = [
+    {
+        NFTId: 1,
+      name: "Splash By DigiverseDawgs",
+    },
+    {
+        NFTId: 2,
+      name: "Proposal Title 2",
+    },
+    {
+        NFTId: 3,
+        name: "Proposal Title 1",
+      },
+      {
+        NFTId: 4,
+        name: "Proposal Title 2",
+      },
+  ];
+  
